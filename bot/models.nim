b028fa5fe0a17bf556134400dfcc5f462292fd83
hamidb80 <hr.bolouri@gmail.com>
2022-12-08 23:57:33 +0330

    add user states

--- bot/models.nim
+++ bot/models.nim
@@ -1,23 +1,48 @@
-import std/times
-import norm/model
+import std/[times, strutils, os, options]
+import norm/[model, sqlite]
+import ndb/sqlite as ns
+
 
 
 type
+  State = enum
+    initial
+    problem
+    answer
+    won
+
+
   User* = ref object of Model
     tgid*: int
     username*: string
     firstname*: string
     lastname*: string
+    state*: State
 
   Puzzle* = ref object of Model
     initial*: string
     log*: string
     shuffled*: string
-    assigned_to*: User
+    assigned_to*: Option[User]
 
-  Answers* = ref object of Model
+  Answer* = ref object of Model
     user*: User
     puzzle*: Puzzle
     input*: string
     timestamp*: Datetime
     is_correct*: bool
+
+
+func dbType*(T: typedesc[enum]): string = "INTEGER"
+func dbValue*(val: enum): DbValue = dbValue val.int
+func to*(dbVal: DbValue, T: typedesc[enum]): T = dbVal.i.T
+
+
+putEnv("DB_HOST", "./test.db")
+let db = getDb()
+
+# TODO pooling
+
+var u1 = User(state: initial)
+db.createTables(Answer(user: User(), puzzle: Puzzle()))
+db.insert(u1)
--- bot/script.nim
+++ bot/script.nim
@@ -4,28 +4,22 @@ when false:
   /start
   -> greeting
 
-  case [yes, no]
-  of yes:
-    -> welcome
+  block loop:
+    case [senMyInputs, wannaAnswer]
+    of 1:
+      -> user.puzzle.shuffled
+      -> user.puzzle.logs
 
-    block loop:
-      case [senMyInputs, wannaAnswer]
-      of 1:
-        -> user.puzzle.shuffled
-        -> user.puzzle.logs
+    of 2:
+      -> doubtSolvedProblemD
+      -> sendToProveD
 
-      of 2:
-        -> doubtSolvedProblemD
-        -> sendToProveD
+      if msg.removeSpaces == user.puzzle.initial.removeSpaces:
+        -> congratsD
+        -> weWillInformYou
 
-        if msg.removeSpaces == user.puzzle.initial.removeSpaces:
-          -> congratsD
-          -> weWillInformYou
-          break loop
+        break loop
 
-        else:
-          -> "no dear, that's not the answer"
-          continue loop
-
-  of no:
-    -> "so sad, maybe next year ..."
+      else:
+        -> "no dear, that's not the answer"
+        continue loop

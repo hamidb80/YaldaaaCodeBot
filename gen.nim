17677bbc04f3774154310a489ed0ffb323e5cf3c
hamidb80 <hr.bolouri@gmail.com>
2022-12-08 09:06:22 +0330

    generate example

--- gen.nim
+++ gen.nim
@@ -1,10 +1,13 @@
-import std/[sequtils, random, sugar, unittest]
+import std/[sequtils, strutils, unicode, random, sugar, unittest]
 
 randomize()
 
 # --- defs
 
-# TODO use utf-8
+type
+  Text = seq[Rune]
+  Record = seq[int]
+  History = seq[Record]
 
 # --- utils
 
@@ -12,15 +15,30 @@ iterator ritems[T](s: openArray[T]): T =
   for i in 1..s.len:
     yield s[^i]
 
+iterator rpairs[T](s: openArray[T]): (int, T) =
+  for i in countdown(s.high, 0):
+    yield (i, s[i])
+
+func toLog(r: Record): string =
+  for i in r:
+    result.add $i
+    result.add ' '
+
+func toLogs(h: History): string =
+  h.map(toLog).join "\n"
+
+func wrap(s: string): string =
+  '"' & s & '"'
+
 # --- core
 
-proc generate(size, n: int): seq[seq[int]] =
-  let indexSeq = toseq 0..<size
+proc genHistory(len, number: int): History =
+  let indexSeq = toseq 0..<len
 
-  for _ in 1..n:
+  for _ in 1..number:
     result.add indexSeq.dup shuffle
 
-func make(sentence: string, records: seq[seq[int]]): string =
+func build(sentence: Text, records: History): Text =
   result = sentence
 
   for r in records:
@@ -28,7 +46,7 @@ func make(sentence: string, records: seq[seq[int]]): string =
     for i, v in r:
       result[i] = acc[v]
 
-func solve(final: string, records: seq[seq[int]]): string =
+func solve(final: Text, records: History): Text =
   result = final
 
   for r in records.ritems:
@@ -40,17 +58,32 @@ func solve(final: string, records: seq[seq[int]]): string =
 
 suite "tests":
   test "solve 1":
-    check solve("ammhin", @[@[3, 0, 2, 1, 5, 4]]) == "mhmani"
+    check solve("ammhin".toRunes, @[@[3, 0, 2, 1, 5, 4]]) == "mhmani".toRunes
 
   test "solve 2":
-    check solve("aimhnm", @[
+    check solve("aimhnm".toRunes, @[
       @[3, 0, 2, 1, 5, 4],
       @[0, 4, 2, 3, 5, 1]
-    ]) == "mhmani"
+    ]) == "mhmani".toRunes
 
   test "generate & solve n":
     let
-      records = generate("hello".len, 10)
-      final = make("hello", records)
+      records = genHistory("hello".len, 10)
+      final = build("hello".toRunes, records)
 
-    check solve(final, records) == "hello"
+    check solve(final, records) == "hello".toRunes
+
+
+when isMainModule:
+  let
+    word = toRunes "شب یلدا"
+    history = genHistory(word.len, 5)
+    final = build(word, history)
+
+  echo wrap $word
+  echo wrap $final
+  echo toLogs history
+  echo "--------------"
+
+  for i, h in history.rpairs:
+    echo toLog h, " -> ", wrap $solve(final, history[i..^1])
